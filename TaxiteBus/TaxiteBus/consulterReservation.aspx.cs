41a4f0ef76ae1cc7109cd471fe3f9d9bcecb4403
Reelie <reelie-maker@hotmail.com>
2017-03-11 15:07:48 -0500

    Page de consultation de réservation

--- TaxiteBus/TaxiteBus/consulterReservation.aspx.cs
+++ TaxiteBus/TaxiteBus/consulterReservation.aspx.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Newtonsoft.Json;
 using TaxiteBus.Models;
 
 namespace TaxiteBus
@@ -16,6 +18,21 @@ namespace TaxiteBus
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            lstReserves = ChargerReservationJSON();
         }
+
+        protected List<Reservation> ChargerReservationJSON()
+        {
+            if (File.Exists("reservations.json"))
+            {
+                string json = System.IO.File.ReadAllText("reservations.json");
+                return JsonConvert.DeserializeObject<List<Reservation>>(json);
+            }
+            else
+            {
+                return new List<Reservation>();
+            }
+        }
+
     }
 }
